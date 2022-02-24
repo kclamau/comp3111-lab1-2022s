c8eb896b9744ffdda7e779bc7f094f1ddaeeeae0
admin <admin@DESKTOP-DDUFIFI>
2022-02-24 23:38:36 +0800

    Initial project setup

---
+++ src/main/java/Lab1/Library.java
@@ -0,0 +1,11 @@
+package Lab1;
+
+public class Library {
+	
+	public static void main(String arg[]) {
+		System.out.println("Hellow, World!");
+	}
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
